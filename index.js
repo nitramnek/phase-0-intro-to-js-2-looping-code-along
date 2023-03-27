5cb5ee621cceb1570550d4aa0e6d25ab0fea0ef3
Kenneth <nitramnek@gmail.com>
2023-03-27 15:33:38 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+//K3n5c0d3
 // Code your solutions in this file
+// For loop
+
+const personality = (["Guadalupe", "Ollie", "Aki"]);
+const thankYouMessages = [];
+
+function writeCards(personality) 
+    {
+        for (let i = 0; i < personality.length; i++) 
+            {
+                thankYouMessages.push(`Thank you, ${personality[i]}, for the wonderful surprise gift!`);
+            }
+            return thankYouMessages;
+    }
+writeCards();
+//debugger;
+function countDown(number) 
+    {
+        for (let i = number; i >= 0; i--) 
+            {
+            console.log(i);
+            }
+    }
+//debugger;
---
+++ indexClass.js
@@ -0,0 +1,29 @@
+//K3n5c0d3
+// Code your solutions in this file
+
+// For loop
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+
+  }
+
+// For with Arrays  
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+//whiteCard
+writeCards (["Ada", "Brendan", "Ali"], "birthday");
+console.log(writeCards);
+
